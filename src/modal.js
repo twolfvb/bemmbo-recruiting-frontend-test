7baf270a91e01188aab1bf9558a89d559c38413e
Tomás Wolf <tomas.wolf.vb@gmail.com>
2024-06-06 11:24:20 -0400

    feat(modal): add new modal with code from https://gist.github.com/constgenius/eac305a93feeece5be317304caf0d28b

---
+++ src/modal.js
@@ -0,0 +1,13 @@
+export default function Modal({ open, onClose, children }) {
+      return (
+          <div onClick={onClose}
+              className={`fixed inset-0 flex justify-center items-center transition-colors ${open ? "visible bg-black/20" : "invisible"}`}
+          > 
+              <div
+                  onClick={(e) => e.stopPropagation()}
+                  className={`bg-white rounded-xl shadow p-6 transition-all ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}>
+                  {children}
+              </div>
+          </div>
+      )
+  }
