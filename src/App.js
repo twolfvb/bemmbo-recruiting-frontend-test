9688791d09f436bf9ef6138b3faaddd52737e003
Tomás Wolf <tomas.wolf.vb@gmail.com>
2024-06-06 10:08:55 -0400

    feat(invoices): load and show invoices (as json)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { getPendingInvoices } from './api/bemmbo.js';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [invoices, setInvoices] = useState([]);
+  useEffect(() => {
+    async function fetchInvoices() {
+      const data = await getPendingInvoices();
+      setInvoices(data);
+    }
+    fetchInvoices();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          {JSON.stringify(invoices)}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
