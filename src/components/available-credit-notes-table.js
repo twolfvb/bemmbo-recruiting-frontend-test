e3c6437540f919b6a584e97d428eb51d3075f83c
Tomás Wolf <tomas.wolf.vb@gmail.com>
2024-06-06 12:11:01 -0400

    refactor(bemmbo-invoice-row): move InvoiceRow to its own component as it was duplicated almost entirely

--- src/components/available-credit-notes-table.js
+++ src/components/available-credit-notes-table.js
@@ -1,38 +1,4 @@
-import { USD_TO_CLP } from '../utils/currency';
-import round from '../utils/round';
-
-function InvoiceRow({ invoice, isSelected, onSelect }) {
-    const handleRadioChange = (e) => {
-      // Prevent the event from propagating to the row's onClick handler
-      e.stopPropagation();
-      onSelect();
-    };
-
-    const clpAmount = invoice.currency === 'USD' ? invoice.amount * USD_TO_CLP : invoice.amount;
-    const usdAmount = invoice.currency === 'CLP' ? invoice.amount / USD_TO_CLP : invoice.amount;
-
-
-    return (
-      <tr className={`text-gray-600 cursor-pointer ${isSelected ? 'bg-indigo-100' : ''}`} onClick={onSelect}>
-        <td className="flex flex-row items-center py-4 px-4" >
-          <input type="radio" checked={isSelected} onChange={handleRadioChange}/>
-          <p className="text-gray-800 font-bold text-left pl-4">
-            {invoice.id}
-            <span className="text-gray-400 font-semibold"> ({invoice.organization_id}) </span>
-          </p>
-        </td>
-        <td className="py-4 px-4">
-          <p>
-            <span className="text-gray-600 font-bold"> ${round(clpAmount)} CLP </span>
-            <span className="text-gray-400 font-semibold"> (${round(usdAmount)} USD) </span>
-          </p>
-        </td>
-        <td className="py-4 px-4">
-          {invoice.reference}
-        </td>
-      </tr>
-    );
-  }
+import InvoiceRow from "./bemmbo-invoice-row";
   
 function ReceivedCreditNotesTable({ creditNotes, selectedCreditNote, setSelectedCreditNote }) {
     return (
